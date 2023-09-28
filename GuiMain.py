db0e25c320b4b0bc0366ca84ff7e8f211e59baf8
KevConrad <kevconrad1988@gmail.com>
2023-09-28 20:19:58 +0200

    Added create and open project dialogs.

--- GuiMain.py
+++ GuiMain.py
@@ -1,4 +1,28 @@
 from tkinter import *
+from tkinter import filedialog as fd
+
+def createNewProject():
+    filetypes = (
+        ('ROM file (SFC format)', '*.sfc'),
+        ('ROM file (SMC format)', '*.smc')
+    )
+
+    filename = fd.askopenfilename(
+        title='Open Illusion of Gaia ROM file',
+        initialdir='/',
+        filetypes=filetypes)
+    return
+
+def openProject():
+    filetypes = (
+        ('GaiaTheCreator project file', '*.gtc'),
+    )
+
+    filename = fd.askopenfilename(
+        title='Open GaiaTheCreator project file',
+        initialdir='/',
+        filetypes=filetypes)
+    return
 
 root = Tk()
 
@@ -12,6 +36,9 @@ menu_file = Menu(menubar)
 menu_edit = Menu(menubar)
 menubar.add_cascade(menu=menu_file, label='File')
 menubar.add_cascade(menu=menu_edit, label='Edit')
+
+menu_file.add_command(label='Create new project...', command=createNewProject)
+menu_file.add_command(label='Open project...', command=openProject)
 
 root.wm_title("GaiaTheCreator")
 
